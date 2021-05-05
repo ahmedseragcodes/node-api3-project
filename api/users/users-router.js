9b0e516d127604ea304aebb846f27d54ac2332f8
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-05 19:02:00 -0400

    wrote routers for accessing users API as well as posts belonging to those users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -6,42 +6,128 @@ const Posts = require("../posts/posts-model");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+//ENDPOINTS
+
+//[GET] All Users
+
+router.get('/', logger, (req, res) => {
+ 
+  Users.get()
+  .then((user)=>{
+    res.status(200).json(user);
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message});
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+//[GET] User By Id
+
+router.get('/:id', logger, validateUserId, (req, res) => {
+ 
+  const { id }=req.params;
+
+  Users.getById(id)
+  .then((user)=>{
+    res.status(200).json(user);
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message});
+  })
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+//[POST] New User
+
+router.post('/', logger, validateUser, (req, res) => {
+  
+  Users.insert(req.body)
+  .then((newUser)=>{
+    res.status(201).json(newUser);
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message});
+  })
 });
+
+//[PUT] / UPDATE User By Id
 
 router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+  
+  const { id }=req.params;
+  const changes = req.body;
+
+Users.update(id, changes)
+.then((recordsUpdated)=>{
+  if (recordsUpdated>0){
+    return Users.getById(id);
+  } else {
+    res.status(404).json({message: "No records updated and/or no records found matching id"});
+  }
+})
+.then((updatedUser)=>{
+  res.status(200).json(updatedUser);
+})
+.catch((err)=>{
+  res.status(500).json({message: err.messsage});
+})
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+//[DELETE] User By Id
+
+router.delete('/:id', logger, validateUserId, (req, res) => {
+ 
+  const { id }=req.params;
+
+  Users.remove(id)
+  .then((recordsDeleted)=>{
+    if (recordsDeleted > 0){
+      res.status(200).json({message: "All Records Matching ID Successfully Deleted"})
+    } else {
+      res.status(404).json({message: "Unable to find any records matching id to delete"})
+    }
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message})
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+//[GET] Specific User's Posts
+
+router.get('/:id/posts', logger, validateUserId, (req, res) => {
+  
+  const { id }=req.params;
+  
+  Users.getUserPosts(id)
+  .then((posts)=>{
+    if (!posts){
+      res.status(404).json({message: "No posts found for user"});
+    } else {
+      res.status(200).json(posts);
+    }
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+//[POST] New Post For Specific User
+
+router.post('/:id/posts', logger, validateUserId, validatePost, (req, res) => {
+ 
+  const { id }=req.params;
+
+  const postBody = { ...req.body, user_id: id };
+
+  Posts.insert(postBody)
+  .then((newPost)=>{
+    res.status(201).json(newPost);
+  })
+  .catch((err)=>{
+    res.status(500).json({message: err.message});
+  })
+
 });
 
-// do not forget to export the router
+
 
 module.exports = router;
